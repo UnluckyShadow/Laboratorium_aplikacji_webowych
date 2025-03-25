6a26c8162f4310b0e68dc698d49ad743836ab5f6
Mateusz Szewczyk <mateusz_sz2003@o2.pl>
2025-03-25 03:34:33 +0100

    Usunięcie przycyisku

--- app/templates_c/9f2bdc577fe5b21f715452e84f0d942bb44f265b_0.file_calc.html.php
+++ app/templates_c/9f2bdc577fe5b21f715452e84f0d942bb44f265b_0.file_calc.html.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 5.4.2, created on 2025-03-25 03:09:43
+/* Smarty version 5.4.2, created on 2025-03-25 03:33:51
   from 'file:E:\Programy\xamp\htdocs\php_04_szablony_smarty/app/calc.html' */
 
 /* @var \Smarty\Template $_smarty_tpl */
 if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   'version' => '5.4.2',
-  'unifunc' => 'content_67e21067ed56a5_11211289',
+  'unifunc' => 'content_67e2160f0d6a29_29815975',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '9f2bdc577fe5b21f715452e84f0d942bb44f265b' => 
     array (
       0 => 'E:\\Programy\\xamp\\htdocs\\php_04_szablony_smarty/app/calc.html',
-      1 => 1742868582,
+      1 => 1742870028,
       2 => 'file',
     ),
   ),
@@ -20,23 +20,23 @@ if ($_smarty_tpl->getCompiled()->isFresh($_smarty_tpl, array (
   array (
   ),
 ))) {
-function content_67e21067ed56a5_11211289 (\Smarty\Template $_smarty_tpl) {
+function content_67e2160f0d6a29_29815975 (\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = 'E:\\Programy\\xamp\\htdocs\\php_04_szablony_smarty\\app';
 $_smarty_tpl->getInheritance()->init($_smarty_tpl, true);
 ?>
 
 
 <?php 
-$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_72679963167e21067e95d28_86573849', 'footer');
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_40272000867e2160f0bbd74_32832571', 'footer');
 ?>
 
 
 <?php 
-$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_198335185267e21067e98165_72015354', 'content');
+$_smarty_tpl->getInheritance()->instanceBlock($_smarty_tpl, 'Block_104931664667e2160f0c0881_16922455', 'content');
 $_smarty_tpl->getInheritance()->endChild($_smarty_tpl, "../templates/main.html", $_smarty_current_dir);
 }
 /* {block 'footer'} */
-class Block_72679963167e21067e95d28_86573849 extends \Smarty\Runtime\Block
+class Block_40272000867e2160f0bbd74_32832571 extends \Smarty\Runtime\Block
 {
 public function callBlock(\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = 'E:\\Programy\\xamp\\htdocs\\php_04_szablony_smarty\\app';
@@ -46,7 +46,7 @@ przykładowa tresć stopki wpisana do szablonu głównego z szablonu kalkulatora
 }
 /* {/block 'footer'} */
 /* {block 'content'} */
-class Block_198335185267e21067e98165_72015354 extends \Smarty\Runtime\Block
+class Block_104931664667e2160f0c0881_16922455 extends \Smarty\Runtime\Block
 {
 public function callBlock(\Smarty\Template $_smarty_tpl) {
 $_smarty_current_dir = 'E:\\Programy\\xamp\\htdocs\\php_04_szablony_smarty\\app';
@@ -80,7 +80,6 @@ $_smarty_current_dir = 'E:\\Programy\\xamp\\htdocs\\php_04_szablony_smarty\\app'
                     <div class="col-12">
                         <ul class="actions special">
                             <li><input type="submit" class="button" value="Oblicz" /></li>
-                            <li><input type="reset" class="button alt" value="Wyczyść formularz" /></li>
                         </ul>
                     </div>
                 </div>
